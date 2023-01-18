1040f74d977f2dc61f323c4692433e5e35202b86
Marcell Martini <marcellmartini@gmail.com>
2023-01-18 11:48:32 -0400

    feat: raindrops exercise's first iteration

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,5 +1,25 @@
 package raindrops
 
+import (
+	"strconv"
+)
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var s string
+
+	if number%3 == 0 {
+		s += "Pling"
+	}
+	if number%5 == 0 {
+		s += "Plang"
+	}
+	if number%7 == 0 {
+		s += "Plong"
+	}
+
+	if s == "" {
+		return strconv.Itoa(number)
+	}
+
+	return s
 }
