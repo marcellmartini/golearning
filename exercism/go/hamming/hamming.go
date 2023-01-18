42359c2c6dee07dc87d58330df47fcd3b6cf71dc
Marcell Martini <marcellmartini@gmail.com>
2023-01-18 10:55:57 -0400

    feat: My solution to hamming exercise.

--- exercism/go/hamming/hamming.go
+++ exercism/go/hamming/hamming.go
@@ -1,5 +1,19 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	var count int
+
+	if len(a) != len(b) {
+		return -1, errors.New("lengths of sequences are differents")
+	}
+
+	for k, v := range a {
+		if v != rune(b[k]) {
+			count++
+		}
+	}
+
+	return count, nil
 }
