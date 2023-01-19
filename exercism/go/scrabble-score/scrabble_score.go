0f0fa5c04de6783606fc5f3de8865c10b9ee38a3
Marcell Martini <marcellmartini@gmail.com>
2023-01-19 12:06:49 -0400

    feat: Third iteration of the scrabble score' solution.
    The solution with 'rune' is faster than with 'string' and 'map'.
    It is the quickest solution of all three
    
    goos: linux
    goarch: amd64
    pkg: scrabble
    cpu: AMD Ryzen 9 5950X 16-Core Processor
    BenchmarkScore
    BenchmarkScore-32 5072797 241.0 ns/op 0 B/op 0 allocs/op
    PASS
    ok      scrabble        1.464s

--- exercism/go/scrabble-score/scrabble_score.go
+++ exercism/go/scrabble-score/scrabble_score.go
@@ -1,22 +1,29 @@
-// Package scrabble scores a scrabble word.
 package scrabble
 
-import "unicode"
+import (
+	"unicode"
+)
 
-var lookup = map[rune]int{
-	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
-	'd': 2, 'g': 2,
-	'b': 3, 'c': 3, 'm': 3, 'p': 3,
-	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
-	'k': 5,
-	'j': 8, 'x': 8,
-	'q': 10, 'z': 10,
-}
+func Score(word string) int {
+	var score int
 
-// Score takes a word and returns its scrabble score.
-func Score(word string) (score int) {
-	for _, ltr := range word {
-		score += lookup[unicode.ToLower(ltr)]
+	for _, v := range word {
+		switch unicode.ToUpper(v) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score += 1
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
 	}
 	return score
 }
