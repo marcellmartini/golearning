dc7d710caf3f23d778d99111a7a66e3dcc363a0d
Marcell Martini <marcellmartini@gmail.com>
2023-01-19 11:48:20 -0400

    feat: first iteraction of resolution of scrabble score.
    The solution with string is slower. Too many memory operations
    
    goos: linux
    goarch: amd64
    pkg: scrabble
    cpu: AMD Ryzen 9 5950X 16-Core Processor
    BenchmarkScore
    BenchmarkScore-32 772519 1723 ns/op 64 B/op 64 allocs/op
    PASS
    ok      scrabble        1.350s

--- exercism/go/scrabble-score/scrabble_score.go
+++ exercism/go/scrabble-score/scrabble_score.go
@@ -1,5 +1,30 @@
 package scrabble
 
+import (
+	"strings"
+)
+
 func Score(word string) int {
-	panic("Please implement the Score function")
+	var score int
+
+	for _, v := range word {
+		switch strings.ToUpper(string(v)) {
+		case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
+			score += 1
+		case "D", "G":
+			score += 2
+		case "B", "C", "M", "P":
+			score += 3
+		case "F", "H", "V", "W", "Y":
+			score += 4
+		case "K":
+			score += 5
+		case "J", "X":
+			score += 8
+		case "Q", "Z":
+			score += 10
+		}
+	}
+
+	return score
 }
