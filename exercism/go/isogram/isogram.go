81c039b2164f9c0d721fe4525629e1450d897109
Marcell Martini <marcellmartini@gmail.com>
2023-01-19 18:36:47 -0400

    feat: First iteration of the isogram' solution.

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,5 +1,20 @@
 package isogram
 
+import (
+	"unicode"
+)
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	var countRune = map[rune]int{}
+
+	for _, v := range word {
+		if v != ' ' && v != '-' {
+			lr := unicode.ToLower(v)
+			if countRune[lr] += 1; countRune[lr] > 1 {
+				return false
+			}
+		}
+	}
+
+	return true
 }
