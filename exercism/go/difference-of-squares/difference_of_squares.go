9b162d1dabbadea6d0144a79ee2976826e6147a3
Marcell Martini <marcellmartini@gmail.com>
2023-01-20 10:53:53 -0400

    feat: First iteration of the difference of squares' solution.

--- exercism/go/difference-of-squares/difference_of_squares.go
+++ exercism/go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,25 @@
 package diffsquares
 
+import "math"
+
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	var count int
+
+	for i := 0; i < n; i++ {
+		count += i + 1
+	}
+	return int(math.Pow(float64(count), 2))
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	var count float64
+
+	for i := 0; i < n; i++ {
+		count += math.Pow(float64(i+1), 2)
+	}
+	return int(count)
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
