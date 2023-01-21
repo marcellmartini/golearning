d36fcda90e133daeea6c40621abf22d7fbe7934a
Marcell Martini <marcellmartini@gmail.com>
2023-01-21 14:45:05 -0400

    feat: First iteration of the grains ' solution.
    Using shift operators

--- exercism/go/grains/grains.go
+++ exercism/go/grains/grains.go
@@ -1,9 +1,17 @@
 package grains
 
+import (
+	"errors"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number < 1 || number > 64 {
+		return 0, errors.New("Not a valid number")
+	}
+
+	return 1 << (number - 1), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	return 1<<64 - 1
 }
