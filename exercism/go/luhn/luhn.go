f76594b9fe82aaeeda0b2f2bf47a19c4126581ca
Marcell Martini <marcellmartini@gmail.com>
2023-01-20 20:38:06 -0400

    feat: First iteration of the Luhn algorithm' solution.

--- exercism/go/luhn/luhn.go
+++ exercism/go/luhn/luhn.go
@@ -1,5 +1,31 @@
 package luhn
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	var op2 int
+	id = strings.ReplaceAll(id, " ", "")
+	if len(id) <= 1 {
+		return false
+	}
+	j := 0
+	for i := len(id) - 1; i >= 0; i-- {
+		tempInt, err := strconv.Atoi(string(id[i]))
+		if err != nil {
+			return false
+		}
+		if j%2 != 0 {
+			if tempInt*2 > 9 {
+				tempInt = tempInt*2 - 9
+			} else {
+				tempInt = tempInt * 2
+			}
+		}
+		op2 += tempInt
+		j++
+	}
+	return (op2 % 10) == 0
 }
